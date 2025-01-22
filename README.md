ad2bb0025ff2d6b9a306131aa65f4089f2571acd
nishu <kumarnishu437@gmail.com>
2025-01-22 14:13:49 +0530

    commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 npx react-native start --reset-cache
 npx react-native build-android --mode=release
 npm run android -- --mode="release"
+npm run android -- --mode="release" --no-install
+./gradlew assembleRelease
+./gradlew assembleDebug
--- src/components/AlertComponent.tsx
+++ src/components/AlertComponent.tsx
@@ -7,53 +7,46 @@ export default function AlertComponent() {
     const { alert, setAlert } = useContext(AlertContext)
 
     return (
-        <View style={styles.container}>
-            <Modal
-                transparent
-                animationType="fade"
-                visible={alert ? true : false}
-                onRequestClose={() => setAlert(undefined)}
-            >
-                {alert && <>
-                    {alert.type == 'snack' ?
+        <Modal
+            transparent
+            animationType="fade"
+            visible={alert ? true : false}
+            onRequestClose={() => setAlert(undefined)}
+        >
+            {alert && <>
+                {alert.type == 'snack' ?
+                    <View style={styles.modalOverlay}>
+                        <Snackbar style={[{
+                            backgroundColor: alert.color == 'error' ? 'red' : 'green'
+                        }]}
+                            visible={!!alert}
+                            onDismiss={() => setAlert(undefined)}
+                            action={{
+                                label: 'Close',
+                                onPress: () => setAlert(undefined),
+                            }}
+                            duration={3000}
+                        >
+                            {alert.message}
+                        </Snackbar>
+                    </View> :
+                    <>
                         <View style={styles.modalOverlay}>
-                            <Snackbar style={[{
-                                backgroundColor: alert.color == 'error' ? 'red' : 'green'
-                            }]}
-                                visible={!!alert}
-                                onDismiss={() => setAlert(undefined)}
-                                action={{
-                                    label: 'Close',
-                                    onPress: () => setAlert(undefined),
-                                }}
-                                duration={3000}
-                            >
-                                {alert.message}
-                            </Snackbar>
-                        </View> :
-                        <>
-                            <View style={styles.modalOverlay}>
-                                <View style={[alert.color == 'error' ? styles.errorAlertBox : styles.successAlertBox, { borderRadius: 10 }]}>
-                                    <Text style={styles.alertTitle}>Alert</Text>
-                                    <Text style={styles.alertMessage}>{alert.message}</Text>
-                                    <Button labelStyle={{ color: 'white' }} onPress={() => setAlert(undefined)} >OK</Button>
-                                </View>
+                            <View style={[alert.color == 'error' ? styles.errorAlertBox : styles.successAlertBox, { borderRadius: 10 }]}>
+                                <Text style={styles.alertTitle}>Alert</Text>
+                                <Text style={styles.alertMessage}>{alert.message}</Text>
+                                <Button labelStyle={{ color: 'white' }} onPress={() => setAlert(undefined)} >OK</Button>
                             </View>
-                        </>
-                    }
-                </>}
-            </Modal>
-        </View>
+                        </View>
+                    </>
+                }
+            </>}
+        </Modal>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex:1000
-    },
+
     modalOverlay: {
         flex: 1,
         justifyContent: 'center',
